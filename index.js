7a7e4b1f26233902f712506fa1db3c5a4536dc26
salman936-wq <salmansardar936@gmail.com>
2026-06-27 16:52:30 +0600

    server started

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_DB_URI
+
+const app = express();
+const port = process.env.PORT;
+const cors = require('cors');
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Server working');
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
