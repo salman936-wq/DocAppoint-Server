160495c902a00a76f25dfa6f382c6b5629bdb051
salman936-wq <salmansardar936@gmail.com>
2026-06-27 19:21:49 +0600

    server seup completed

--- index.js
+++ index.js
@@ -11,10 +11,35 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json())
 
-
 app.get('/', (req, res) => {
   res.send('Server working');
 });
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('DocAppoint')
+    const doctorsCollection = db.collection("doctors")
+
+
+    app.get("/doctors", async (req, res) => {
+      const doctorsData = await doctorsCollection.find().toArray();
+      res.send(doctorsData)
+    })
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
